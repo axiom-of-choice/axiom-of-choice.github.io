c4e8664edfbc2c57dde00833ae01c7f41f8da7db
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2021-10-30 12:30:32 -0500

    changes in the app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     print("Handling request to home page")
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='localhost')
