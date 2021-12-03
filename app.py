a20e6bcfeffd5fff07d449d0a2267d5a9afe214f
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2021-12-03 10:41:48 -0600

    New route added, also changed work experience

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,6 +6,11 @@ def index():
     print("Handling request to home page")
     return render_template('index.html')
 
-
+@app.route("/me", methods=["GET", "POST"])
+def me():
+    if request.method == "POST":
+        return "Method not available"
+    else:
+        return render_template("me.html")
 if __name__ == '__main__':
     app.run()
